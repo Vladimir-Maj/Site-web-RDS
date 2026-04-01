0c01bc27c1359905d09b8b8085b3f0ebd2748fba
Turker CALISKAN <turker.caliskan@viacesi.fr>
2026-04-01 14:38:04 +0200

    Update BuisnessReviewModel.php
    ## Global
    Ajout support pour le nouveau scheme

--- www/prod/.back/models/BuisnessReviewModel.php
+++ www/prod/.back/models/BuisnessReviewModel.php
@@ -2,35 +2,38 @@
 namespace App\Models;
 
 use DateTime;
-class BuisnessReviewModel 
+
+class BuisnessReviewModel
 {
-    public string $business_id;
-    public string $reviewer_id;
+    public int $company_id_business_review;
+    public int $pilot_id_business_review;
     public string $compound_id;
-    public string $rating;
-    public string $comment;
-    public DateTime $review_date;
+    public int $rating_business_review;
+    public string $comment_business_review;
+    public DateTime $reviewed_at_business_review;
 
     public function __construct()
     {
-        // Initialize properties with default values if needed
-        $this->business_id = '';
-        $this->reviewer_id = '';
+        $this->company_id_business_review = 0;
+        $this->pilot_id_business_review = 0;
         $this->compound_id = '';
-        $this->rating = '';
-        $this->comment = '';
-        $this->review_date = new DateTime();
+        $this->rating_business_review = 0;
+        $this->comment_business_review = '';
+        $this->reviewed_at_business_review = new DateTime();
     }
 
     public static function fromArray(array $data): self
     {
         $instance = new self();
-        $instance->business_id = $data['business_id'] ?? '';
-        $instance->reviewer_id = $data['reviewer_id'] ?? '';
+        $instance->company_id_business_review = (int) ($data['company_id_business_review'] ?? ($data['business_id'] ?? 0));
+        $instance->pilot_id_business_review = (int) ($data['pilot_id_business_review'] ?? ($data['reviewer_id'] ?? 0));
         $instance->compound_id = $data['compound_id'] ?? '';
-        $instance->rating = $data['rating'] ?? '';
-        $instance->comment = $data['comment'] ?? '';
-        $instance->review_date = isset($data['review_date']) ? new DateTime($data['review_date']) : new DateTime();
+        $instance->rating_business_review = (int) ($data['rating_business_review'] ?? ($data['rating'] ?? 0));
+        $instance->comment_business_review = $data['comment_business_review'] ?? ($data['comment'] ?? '');
+        $instance->reviewed_at_business_review = isset($data['reviewed_at_business_review'])
+            ? new DateTime($data['reviewed_at_business_review'])
+            : (isset($data['review_date']) ? new DateTime($data['review_date']) : new DateTime());
+
         return $instance;
     }
 }
