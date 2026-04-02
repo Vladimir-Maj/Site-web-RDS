436a37dc295831f7bec3b0f033a47b758bb1ef05
Spectre9813 <arthurchant2006@gmail.com>
2026-04-02 12:27:52 +0200

    Refactor review date mapping in fromArray method
    Refactor fromArray method to improve review date mapping.

--- www/prod/.back/models/BuisnessReviewModel.php
+++ www/prod/.back/models/BuisnessReviewModel.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace App\Models;
 
 use DateTime;
+use Exception;
 
 class BuisnessReviewModel
 {
@@ -22,17 +24,37 @@ class BuisnessReviewModel
         $this->reviewed_at_business_review = new DateTime();
     }
 
+    /**
+     * Maps an array to the Model. 
+     * Supports both the new {attribute}_{table} schema and the legacy format.
+     */
     public static function fromArray(array $data): self
     {
         $instance = new self();
+
+        // Map Company/Business ID
         $instance->company_id_business_review = (int) ($data['company_id_business_review'] ?? ($data['business_id'] ?? 0));
+
+        // Map Pilot/Reviewer ID
         $instance->pilot_id_business_review = (int) ($data['pilot_id_business_review'] ?? ($data['reviewer_id'] ?? 0));
+
+        // Map Compound ID (Shared)
         $instance->compound_id = $data['compound_id'] ?? '';
+
+        // Map Rating
         $instance->rating_business_review = (int) ($data['rating_business_review'] ?? ($data['rating'] ?? 0));
+
+        // Map Comment
         $instance->comment_business_review = $data['comment_business_review'] ?? ($data['comment'] ?? '');
-        $instance->reviewed_at_business_review = isset($data['reviewed_at_business_review'])
-            ? new DateTime($data['reviewed_at_business_review'])
-            : (isset($data['review_date']) ? new DateTime($data['review_date']) : new DateTime());
+
+        // Map Review Date
+        if (isset($data['reviewed_at_business_review'])) {
+            $instance->reviewed_at_business_review = new DateTime($data['reviewed_at_business_review']);
+        } elseif (isset($data['review_date'])) {
+            $instance->reviewed_at_business_review = new DateTime($data['review_date']);
+        } else {
+            $instance->reviewed_at_business_review = new DateTime();
+        }
 
         return $instance;
     }
