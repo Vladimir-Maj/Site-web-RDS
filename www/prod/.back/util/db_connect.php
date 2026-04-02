ef27a3269e1e3c554d91c8f9ef6bbd5c0b53a605
Spectre9813 <arthurchant2006@gmail.com>
2026-04-02 21:55:57 +0200

    [PATCH 06/13] config: Update router, DB connection & ignore patterns

--- www/prod/.back/util/db_connect.php
+++ www/prod/.back/util/db_connect.php
@@ -2,11 +2,11 @@
 // prod/.back/util/db_connect.php
 
 // ===== LOCAL (Docker) =====
-$host = '127.0.0.1';
+$host = 'db';
 $port = 3306;
 $db   = 'sql_db';
-$user = 'root';
-$pass = 'Vlad123MotDePasse!';
+$user = 'website-local';
+$pass = '1234';
 
 /*
 // ===== PRODUCTION =====
